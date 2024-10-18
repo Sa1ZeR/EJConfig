407a33635cba352029c540b2bcc3266ec7746c65
Serg1K <amsher2007@mail.ru>
2024-10-18 23:04:21 +1000

    fix file type

--- src/main/kotlin/com/sazer/ejconfig/utils/FileUtils.kt
+++ src/main/kotlin/com/sazer/ejconfig/utils/FileUtils.kt
@@ -9,7 +9,7 @@ object FileUtils {
     @JvmStatic
     fun getFileType(path: String): ConfigType {
         val splitData = path.split(".")
-        if(splitData.size < 1) throw IllegalArgumentException("File is not valid")
+        if(splitData.size <= 1) throw IllegalArgumentException("File is not valid")
 
         return when (splitData[splitData.size - 1]) {
             "yml" -> ConfigType.YAML
